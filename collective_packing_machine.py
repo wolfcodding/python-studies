ab8889e84722d737b421b9b4c63921e3bebb6b8b
Rafal Lesniak <ralf.lesniak@gmail.com>
2025-08-08 16:10:17 +0100

    Add files via upload

---
+++ collective_packing_machine.py
@@ -0,0 +1,43 @@
+'''Program for packing warehouse prodcucts indivudually and then collectivly
+
+    * code uses decorator stacking
+'''
+
+def big_container(collective_material):
+    '''packs boxes into the collective material passed'''
+    def wrapper(our_function):
+        def internal_wrapper(*args):
+            our_function(*args)
+            print('<strong>*</strong> The whole order would be packed with', collective_material)
+            print()
+        return internal_wrapper
+    return wrapper
+
+def warehouse_decorator(material):
+    '''wraps single items into different materials'''
+    def wrapper(our_function):
+        def internal_wrapper(*args):
+            our_function(*args)
+            print('<strong>*</strong> Wrapping items from {} with {}'.format(our_function.__name__, material))
+        return internal_wrapper
+    return wrapper
+
+@big_container('plain cardboard')
+@warehouse_decorator('bubble foil')
+def pack_books(*args):
+    print("We'll pack books: ", args)
+
+@big_container('colourful cardboard')
+@warehouse_decorator('foil')
+def pack_toys(*args):
+    print("We'll pack toys:", args)
+
+@big_container('strong cardboard')
+@warehouse_decorator('cardboard')
+def pack_fruits(*args):
+    print("We'll pack fruits:", args)
+
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
